b57113e23388e593337548bc60f341ba84c20ed5
kieran123558 <kkhor@parrastu.catholic.edu.au>
2024-11-11 22:23:36 +1100

    w4 replacing user email with nicknames, including refactor invitations email notifications

--- chipin/views.py
+++ chipin/views.py
@@ -68,12 +68,30 @@ def invite_users(request, group_id):
         user_id = request.POST.get('user_id')
         invited_user = get_object_or_404(User, id=user_id)      
         if invited_user in group.invited_users.all():
-            messages.info(request, f'{invited_user.username} has already been invited.')
+            messages.info(request, f'{invited_user.profile.nickname} has already been invited.')
         else:
             group.invited_users.add(invited_user)
-            messages.success(request, f'Invitation sent to {invited_user.username}.')
+            messages.success(request, f'Invitation sent to {invited_user.profile.nickname}.')
         return redirect('chipin:group_detail', group_id=group.id)  
     return render(request, 'chipin/invite_users.html', {
         'group': group,
         'users_not_in_group': users_not_in_group
     })
+
+@login_required
+def accept_invite(request, group_id):
+    group = get_object_or_404(Group, id=group_id)
+    user_id = request.GET.get('user_id')
+    if user_id:
+        invited_user = get_object_or_404(User, id=user_id)
+        if invited_user in group.members.all():
+            messages.info(request, f'{invited_user.profile.nickname} is already a member of the group "{group.name}".')
+        elif invited_user in group.invited_users.all():
+            group.members.add(invited_user)
+            group.invited_users.remove(invited_user)  # Remove from invited list
+            messages.success(request, f'{invited_user.profile.nickname} has successfully joined the group "{group.name}".')
+        else:
+            messages.error(request, "You are not invited to join this group.")
+    else:
+        messages.error(request, "Invalid invitation link.")  
+    return redirect('chipin:group_detail', group_id=group.id)
